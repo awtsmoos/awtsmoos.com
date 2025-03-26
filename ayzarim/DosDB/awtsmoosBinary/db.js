c89af930f7282e3184b25ac94870c46f46bdedc7
awtsmoos <cobykaufer@gmail.com>
2025-03-26 04:54:55 -0400

    B"H More hash based database stuff

--- ayzarim/DosDB/awtsmoosBinary/db.js
+++ ayzarim/DosDB/awtsmoosBinary/db.js
@@ -1,79 +1,246 @@
 //B"H
 var path = require("path");
-var fs = require("fs").promises;
+var fs = require("fs");
 
 var serializeValue = require("./awtsmoosBinaryJSON/serialize/serializeValue.js");
 var directlyParseValue = require("./awtsmoosBinaryJSON/parsing/direct.js")
 var FileBuffer = require("./fileBuffer");
 var AwtsmoosHashMap = require("./awtsmoosBinaryJSON/helpers/hashing/AwtsmoosHashMap.js")
 var {
-  ensureDir
-}  = require("./helpers.js")
+	ensureDir
+} = require("./helpers.js")
 class AwtsmoosDB {
-  constructor(dbDir, {
-    hashMapInitialCapacity = 8
-  }={}) {
-    this.dir=dbDir || "./awtsmoosDb"
-    this.hashMapCapacity = hashMapInitialCapacity;
-    this.hashEntrySize = 4;
-
-	this.ensureDir();
-  }
-
-  
-
-
-  getHashEntry(key) {
-	var hash = this.hashMap();
-	var rawValue = hash.getValueAtKey(key);
-	var parst = directlyParseValue(rawValue);
+	constructor(dbDir, {
+		hashMapInitialCapacity = 8,
+		shardByteSize = 1024
+	} = {}) {
+		this.dir = dbDir || "./awtsmoosDb"
+		this.hashMapCapacity = hashMapInitialCapacity;
+		this.hashEntrySize = 4;
+		this.shardByteSize = shardByteSize;
+		this.ensureDir();
+	}
 	
-	//console.log("Raw",rawValue,rawValue+"")
-	return parst;
-  }
+	
+	
+	
+	getHashEntry(key) {
 
-  addHashValueToIndex(key, value) {
-	var hash = this.hashMap();
-	var serialized = serializeValue(value);
-	hash.setEntry(key, serialized);
+		var masterIndex = this.getMasterIndex();
 
-  }
+		var masterDataBuffer = this.getMasterIndexValues();
+		var masterMap = new AwtsmoosHashMap({
+			buffer: masterIndex,
+			dataBuffer: masterDataBuffer
+		});
 
+		var masterRaw = masterMap.getValueAtKey(key);
 
-  hashMap() {
-	var mainIndex = path.join(this.dir, "index.db");
-	ensureDir(mainIndex);
-	var buf = new FileBuffer(mainIndex);
+		var parst = directlyParseValue(masterRaw);
+		if(!parst) {
+			return console.log(masterRaw, parst, "PAR")
+		}
+		var shardIdx = parst.shardIdx;
 
-	var firstShard = path.join(
-		this.dir,
-		"shard-0.db"
-	);
-	ensureDir(firstShard);
-	var shardBuffer = new FileBuffer(firstShard);
+		var shardBuffer = this.findShardIfExists(
+			shardIdx
+		);
 
-	var hash = new AwtsmoosHashMap({
-		buffer: buf,
-		dataBuffer: shardBuffer
-	});
-	return hash;
-  }
+		if(!shardBuffer) {
+			return {
+				error: {
+					message: "Couldn't find corresponding shard",
+					details: shardIdx
+				}
+			}
+		}
 
-  async getFile(fPath) {
-    try {
-      return fs.readFile(fPath);
-    } catch(e) {
+		var hash = this.hashMap(shardBuffer);
+		
+		var rawValue = hash.getValueAtKey(key);
+		var parst = directlyParseValue(rawValue);
+		
+		//console.log("Raw",rawValue,rawValue+"")
+		return parst;
+	}
+	
+	addHashValueToIndex(key, value) {
+		
+		var serialized = serializeValue(value);
+		var byteLength = serialized.length;
+		var shardAvailable = this.findShardWithEnoughSpaceInDir(
+			byteLength
+		);
 
-    }
-  }
+		if(shardAvailable.error) {
+			return shardAvailable;
+		}
+		var hash = this.hashMap(shardAvailable);
+		var {
+			offsetInData
+		 } = hash.setEntry(key, serialized);
 
+		var masterIndex = this.getMasterIndex();
+		var masterDataBuffer = this.getMasterIndexValues();
+		
+		var masterMap = new AwtsmoosHashMap({
+			buffer: masterIndex,
+			dataBuffer: masterDataBuffer
+		});
 
+		var serializeAwtsmoos = serializeValue(
+			{
+				offsetInData,
+				shardIdx: shardAvailable.shardIdx
+			}
+		)
 
+		masterMap.setEntry(key, serializeAwtsmoos);
 
-  async ensureDir() {
-    if(!this.dir) return;
-    ensureDir(this.dir)
-  }
+		return {
+			success: serialized
+		};
+
+	}
+
+	getMasterIndexValues() {
+		var masterValues = path.join(
+			this.dir,
+			"master.awts"
+		)
+		try {
+			fs.statSync(
+				masterValues
+			);
+			var fb = new FileBuffer(
+				masterValues
+			);
+			return fb;
+		} catch(e) {
+			ensureDir(masterValues);
+			var fb = new FileBuffer(
+				masterValues
+			)
+			return fb;
+		}
+	}
+	getMasterIndex() {
+		var masterIndexPath = path.join(
+			this.dir,
+			"index.awts"
+		);
+		try {
+			var indexStat = fs.statSync(
+				masterIndexPath
+			);
+			var fb = new FileBuffer(
+				masterIndexPath
+			);
+			return fb;
+		} catch(e) {
+			ensureDir(masterIndexPath);
+			var fb = new FileBuffer(
+				masterIndexPath
+			)
+			return fb;
+		}
+	}
+
+	findShardIfExists(
+		shardIdx
+	) {
+		var shardPath = path.join(
+			this.dir,
+			`shard-${shardIdx}.awts`
+		);
+		try {
+			fs.statSync(shardPath);
+			return {
+				shardBuffer: new FileBuffer(
+					shardPath
+				),
+				shardIdx
+			}
+		} catch(e) {
+			return null;
+		}
+	}
+
+	findShardWithEnoughSpaceInDir(
+		spaceNeeded=0, 
+		shardIdx = 0
+	) {
+		var shardPath = path.join(
+			this.dir,
+			`shard-${shardIdx}.awts`
+		);
+
+		try {
+			
+			var shard = fs.statSync(shardPath);
+			
+			
+			if(shard.size + spaceNeeded < this.shardByteSize) {
+				var shardBuffer = new FileBuffer(shardPath);
+				return {
+					shardBuffer,
+					shardIdx
+				};
+			} else {
+				return this.findShardWithEnoughSpaceInDir(
+					spaceNeeded, shardIdx + 1
+				);
+			}
+		} catch(e) {
+
+			if(e.code == "ENOENT") {
+				ensureDir(shardPath);
+				var shardBuffer = new FileBuffer(shardPath);
+				return {
+					shardBuffer,
+					shardIdx
+				};
+			} else {
+				console.log("THere was a serious error!", e);
+				return {
+					error: e
+				}
+			}
+		}
+	}
+	
+	
+	hashMap({
+		shardBuffer, 
+		shardIdx = 0
+	}={}) {
+		var name = "index-"+shardIdx+".awts"
+		var shardIndex = path.join(this.dir, name);
+		ensureDir(shardIndex);
+		var buf = new FileBuffer(shardIndex);
+		
+		var hash = new AwtsmoosHashMap({
+			buffer: buf,
+			dataBuffer: shardBuffer
+		});
+		return hash;
+	}
+	
+	getFile(fPath) {
+		try {
+			return fs.readFile(fPath);
+		} catch (e) {
+			
+		}
+	}
+	
+	
+	
+	
+	ensureDir(dir=this.dir) {
+		if(this.dir) return;
+		ensureDir(dir)
+	}
 }
 
 module.exports = AwtsmoosDB
