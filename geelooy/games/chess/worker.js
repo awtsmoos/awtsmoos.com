bd786f60922ede888517b7304d3a384be9afd2a2
awtsmoos <118651658+awtsmoos@users.noreply.github.com>
2025-10-17 14:13:50 -0400

    B"H Boruch Hashem! Biezras Hashem Blessed is He At Fri Oct 17 2025 14:13:50 GMT-0400 (Eastern Daylight Time), updated worker.js

--- geelooy/games/chess/worker.js
+++ geelooy/games/chess/worker.js
@@ -41,7 +41,7 @@ let stopSearch = false;
 // AI Data Structures
 let killerMoves = Array(MATE_IN_MAX_PLY).fill(null).map(() => [null, null]);
 let historyTable = Array(12).fill(null).map(() => Array(64).fill(0));
-let transpositionTable = new Map();
+
 const TT_EXACT = 0, TT_LOWERBOUND = 1, TT_UPPERBOUND = 2;
 
 // --- Piece-Square Tables ---
