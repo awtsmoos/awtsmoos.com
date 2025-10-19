a0442e07403c5f909e7e5be20c6a3df83d29a9aa
awtsmoos <118651658+awtsmoos@users.noreply.github.com>
2025-10-19 01:52:23 -0400

    B"H Boruch Hashem! Biezras Hashem Blessed is He At Sun Oct 19 2025 01:52:20 GMT-0400 (Eastern Daylight Time), updated grandmaster_library.js

--- geelooy/games/chess/grandmaster_library.js
+++ geelooy/games/chess/grandmaster_library.js
@@ -169,3 +169,131 @@ openingBook.set(generateHashFromFEN('rnbqkbnr/pppppppp/8/8/5P2/8/PPPP2PP/RNBQKBN
 
 // Larsen's Opening
 openingBook.set(generateHashFromFEN('rnbqkbnr/pppppppp/8/8/8/1P6/P1PPPPPP/RNBQKBNR b KQkq - 0 1'), [ { from: [1, 4], to: [3, 4] } ]);
+
+
+
+// =================================================================
+//      GRANDMASTER'S LIBRARY EXPANSION PACK 1
+// =================================================================
+// This pack adds over 50 crucial new lines and variations.
+
+// --- King's Gambit (1. e4 e5 2. f4) ---
+openingBook.set(generateHashFromFEN('rnbqkbnr/pppp1ppp/8/4p3/4PP2/8/PPPP2PP/RNBQKBNR b KQkq - 0 2'), [ { from: [4, 4], to: [5, 5] }, { from: [7, 5], to: [3, 1] }, { from: [1, 3], to: [3, 3] } ]);
+// King's Gambit Accepted, Main Line (3. Nf3 g5)
+openingBook.set(generateHashFromFEN('rnbqkbnr/pppp1p1p/8/4p1p1/4PP2/5N2/PPPP2PP/RNBQKB1R w KQkq - 0 4'), [ { from: [6, 7], to: [4, 7] }, { from: [7, 5], to: [4, 2] } ]);
+// King's Gambit Accepted, Fischer Defense (3. Nf3 d6)
+openingBook.set(generateHashFromFEN('rnbqkbnr/ppp2p1p/3p4/4p3/4PP2/5N2/PPPP2PP/RNBQKB1R b KQkq - 1 4'), [ { from: [1, 4], to: [3, 4] } ]);
+// Falkbeer Countergambit (2...d5)
+openingBook.set(generateHashFromFEN('rnbqkbnr/pppp1ppp/8/3pp3/4PP2/8/PPPP2PP/RNBQKBNR w KQkq - 0 3'), [ { from: [4, 4], to: [3, 3] } ]);
+
+// --- Scotch Game (1. e4 e5 2. Nf3 Nc6 3. d4) ---
+openingBook.set(generateHashFromFEN('r1bqkbnr/pppp1ppp/2n5/4p3/3PP3/5N2/PPP2PPP/RNBQKB1R b KQkq - 0 3'), [ { from: [4, 4], to: [3, 3] } ]);
+// Main Line after 4. Nxd4
+openingBook.set(generateHashFromFEN('r1bqkbnr/pppp1ppp/2n5/8/3NP3/8/PPP2PPP/RNBQKB1R b KQkq - 0 4'), [ { from: [1, 5], to: [3, 5] }, { from: [7, 5], to: [4, 2] } ]);
+// Scotch Gambit
+openingBook.set(generateHashFromFEN('r1bqkbnr/pppp1ppp/2n5/8/2B1P3/5N2/PB3PPP/RN1QK2R b KQkq - 1 6'), [ { from: [1, 5], to: [3, 5] } ]);
+
+// --- Scandinavian Defense (1. e4 d5) ---
+openingBook.set(generateHashFromFEN('rnbqkbnr/pppppppp/8/3p4/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 2'), [ { from: [4, 4], to: [3, 3] } ]);
+// Main Line after 2. exd5 Qxd5
+openingBook.set(generateHashFromFEN('rnb1kbnr/ppp1pppp/8/3q4/8/2N5/PPPP1PPP/R1BQKBNR b KQkq - 1 3'), [ { from: [3, 3], to: [0, 4] } ]);
+// Modern Variation (2...Nf6)
+openingBook.set(generateHashFromFEN('rnbqkb1r/ppp1pppp/5n2/3p4/3P4/5N2/PPP1PPPP/RNBQKB1R b KQkq - 1 3'), [ { from: [1, 2], to: [3, 2] } ]);
+
+// --- Alekhine's Defense (1. e4 Nf6) ---
+openingBook.set(generateHashFromFEN('rnbqkb1r/pppppppp/5n2/8/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 1 2'), [ { from: [4, 4], to: [3, 4] } ]);
+// Four Pawns Attack
+openingBook.set(generateHashFromFEN('rnbqkb1r/ppp1pppp/5n2/3p4/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 1 3'), [ { from: [1, 5], to: [3, 5] } ]);
+
+// --- Pirc Defense (1. e4 d6 2. d4 Nf6 3. Nc3 g6) ---
+openingBook.set(generateHashFromFEN('rnbqkb1r/ppp1pp1p/3p1np1/8/3PP3/2N5/PPP2PPP/R1BQKBNR w KQkq - 0 4'), [ { from: [7, 5], to: [5, 4] }, { from: [7, 6], to: [5, 5] }, { from: [6, 5], to: [4, 5] } ]);
+// Austrian Attack
+openingBook.set(generateHashFromFEN('rnbqkb1r/ppp1pp1p/3p1np1/8/3PP3/2N2N2/PPP2PPP/R1BQKB1R b KQkq - 1 4'), [ { from: [7, 5], to: [6, 5] } ]);
+
+// --- Ruy Lopez: Berlin Defense (3...Nf6) ---
+openingBook.set(generateHashFromFEN('r1bqkbnr/pppp1ppp/2n2n2/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R w KQkq - 2 4'), [ { from: [7, 7], to: [7, 5] }, { from: [6, 3], to: [4, 3] } ]);
+// The infamous "Berlin Wall" endgame
+openingBook.set(generateHashFromFEN('r1bqkb1r/pppp1ppp/2n2n2/4p3/3PP3/5N2/PPP2PPP/RNBQKB1R b KQkq - 0 4'), [ { from: [4, 4], to: [3, 3] } ]);
+
+// --- Sicilian Defense: Key Anti-Sicilians & Variations ---
+// Alapin Variation (2. c3)
+openingBook.set(generateHashFromFEN('rnbqkbnr/pp1ppppp/8/2p5/4P3/2P5/PP1P1PPP/RNBQKBNR b KQkq - 0 2'), [ { from: [1, 3], to: [3, 3] }, { from: [1, 5], to: [3, 5] } ]);
+// Alapin after 2...d5
+openingBook.set(generateHashFromFEN('rnbqkbnr/pp2pppp/8/2pp4/4P3/2P5/PP1P1PPP/RNBQKBNR w KQkq - 0 3'), [ { from: [4, 4], to: [3, 3] } ]);
+// Smith-Morra Gambit (2. d4 cxd4 3. c3)
+openingBook.set(generateHashFromFEN('rnbqkbnr/pp2pppp/8/3p4/3pP3/2P5/PP3PPP/RNBQKBNR w KQkq - 0 4'), [ { from: [3, 3], to: [2, 2] } ]);
+// Sveshnikov Variation Main Line
+openingBook.set(generateHashFromFEN('r1bqkb1r/1p1n1ppp/p2p1n2/4p3/3NP3/2N1B3/PPP2PPP/R2QKB1R w KQkq - 0 8'), [ { from: [3, 3], to: [1, 2] } ]);
+// Kalashnikov Variation
+openingBook.set(generateHashFromFEN('r1bqkb1r/1p1n1ppp/p2p1n2/4p3/3NP3/2N5/PPP1BPPP/R1BQK2R b KQkq - 1 7'), [ { from: [1, 4], to: [3, 4] } ]);
+// Closed Sicilian
+openingBook.set(generateHashFromFEN('rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR b KQkq - 1 2'), [ { from: [1, 2], to: [3, 2] } ]);
+
+// --- French Defense: Winawer Variation (3. Nc3 Bb4) ---
+openingBook.set(generateHashFromFEN('rnbqkb1r/pppp1ppp/4p3/3nP3/3P4/2N5/PPP2PPP/R1BQKBNR b KQkq - 2 4'), [ { from: [7, 5], to: [3, 1] } ]);
+// Winawer Poisoned Pawn
+openingBook.set(generateHashFromFEN('rnbq1k1r/pp1p1ppp/4pn2/2p1b3/3P4/2N1B3/PPP1QPPP/R3KBNR b KQ - 1 7'), [ { from: [4, 1], to: [3, 2] } ]);
+// Winawer Main Line (7. Qg4)
+openingBook.set(generateHashFromFEN('r1b1k2r/pp1p1ppp/1qn1pn2/2p5/3P2Q1/2N1B3/PPP1PPPP/R3KBNR w KQkq - 4 7'), [ { from: [7, 7], to: [7, 5] } ]);
+
+// --- London System (1. d4 d5 2. Bf4) ---
+openingBook.set(generateHashFromFEN('rnbqkbnr/pppp1ppp/8/4p3/3P1B2/8/PPP1PPPP/RN1QKBNR b KQkq - 1 2'), [ { from: [1, 5], to: [3, 5] }, { from: [1, 2], to: [3, 2] } ]);
+// Standard response 2...Nf6
+openingBook.set(generateHashFromFEN('rnbqkb1r/pppp1ppp/5n2/4p3/3P1B2/8/PPP1PPPP/RN1QKBNR w KQkq - 0 3'), [ { from: [7, 6], to: [5, 5] }, { from: [6, 4], to: [4, 4] } ]);
+// Jobava London (2. Nc3)
+openingBook.set(generateHashFromFEN('rnbqkb1r/pppppppp/5n2/8/3P4/2N5/PPP1PPPP/R1BQKBNR b KQkq - 2 2'), [ { from: [1, 3], to: [3, 3] } ]);
+
+// --- Catalan Opening (1. d4 Nf6 2. c4 e6 3. g3) ---
+openingBook.set(generateHashFromFEN('rnbqkb1r/pppp1ppp/4pn2/8/2PP4/6P1/PP2PP1P/RNBQKBNR b KQkq - 0 3'), [ { from: [1, 3], to: [3, 3] } ]);
+// Open Catalan
+openingBook.set(generateHashFromFEN('rnbqkb1r/pp3ppp/4pn2/2pp4/2PP4/5NP1/PP2PPBP/RNBQK2R b KQkq - 1 6'), [ { from: [3, 3], to: [2, 2] } ]);
+// Closed Catalan
+openingBook.set(generateHashFromFEN('rnbqk2r/pp2bppp/4pn2/2pp4/2PP4/5NP1/PP2PPBP/RNBQK2R w KQkq - 0 7'), [ { from: [7, 7], to: [7, 5] } ]);
+
+// --- King's Indian Defense: Additional Variations ---
+// Sämisch Variation (5. f3)
+openingBook.set(generateHashFromFEN('rnbqk2r/ppp1bppp/3p1n2/4p3/2PP4/2N2N2/PP2PPPP/R1BQKB1R w KQkq - 0 6'), [ { from: [6, 5], to: [4, 5] } ]);
+// Fianchetto Variation (3. g3)
+openingBook.set(generateHashFromFEN('rnbqkb1r/pppppp1p/5np1/8/2PP4/6P1/PP2PP1P/RNBQKBNR b KQkq - 0 3'), [ { from: [7, 5], to: [6, 5] } ]);
+// Four Pawns Attack
+openingBook.set(generateHashFromFEN('rnbqkb1r/pp2pp1p/3p1np1/8/2PNP3/2N5/PP3PPP/R1BQKB1R b KQkq - 1 6'), [ { from: [6, 5], to: [4, 5] } ]);
+
+// --- Queen's Gambit Declined: More Defenses ---
+// Tarrasch Defense (3...c5)
+openingBook.set(generateHashFromFEN('rnbqkbnr/pp2pppp/3p4/2p5/2PP4/8/PP2PPPP/RNBQKBNR w KQkq - 0 3'), [ { from: [3, 3], to: [4, 4] } ]);
+// Cambridge Springs Defense
+openingBook.set(generateHashFromFEN('r1bqk2r/pp1nbppp/2p1pn2/3p2B1/2PP4/2N2N2/PPQ1PPPP/R3KB1R w KQkq - 4 7'), [ { from: [7, 7], to: [7, 5] } ]);
+
+// --- Slav Defense: More Variations ---
+// Chebanenko (Chameleon) Slav (4...a6)
+openingBook.set(generateHashFromFEN('rnbqk2r/p1p1bppp/1p1p1n2/4p3/2PP4/2N2N2/PP2PPPP/R1BQKB1R w KQkq - 0 6'), [ { from: [1, 5], to: [3, 5] } ]);
+
+// --- Nimzo-Indian: Main Alternatives for White ---
+// Classical Variation (4. Qc2)
+openingBook.set(generateHashFromFEN('rnbqk2r/pppp1ppp/4pn2/8/1bPP4/2N5/PPQ1PPPP/R1BQKBNR b KQkq - 1 4'), [ { from: [1, 3], to: [3, 3] }, { from: [1, 2], to: [3, 2] } ]);
+// Leningrad Variation (4. Bg5)
+openingBook.set(generateHashFromFEN('rnbqk2r/pppp1ppp/4pn2/6B1/1bPP4/2N5/PP2PPPP/R2QKBNR b KQkq - 1 4'), [ { from: [1, 7], to: [3, 7] } ]);
+
+// --- Modern Defense (1. e4 g6) ---
+openingBook.set(generateHashFromFEN('rnbqkbnr/pppppp1p/6p1/8/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 2'), [ { from: [6, 3], to: [4, 3] } ]);
+// Averbakh System
+openingBook.set(generateHashFromFEN('rnbqk1nr/ppp1ppbp/3p2p1/8/2BPP3/5N2/PPP2PPP/RNBQK2R b KQkq - 1 4'), [ { from: [1, 5], to: [3, 5] } ]);
+
+// --- Colle System (1. d4 d5 2. Nf3 Nf6 3. e3) ---
+openingBook.set(generateHashFromFEN('rnbqkb1r/ppp1pppp/5n2/3p4/3P4/4PN2/PPP2PPP/RNBQKB1R b KQkq - 1 3'), [ { from: [1, 4], to: [3, 4] }, { from: [1, 2], to: [3, 2] } ]);
+
+// --- Torre Attack (1. d4 Nf6 2. Nf3 e6 3. Bg5) ---
+openingBook.set(generateHashFromFEN('rnbqkb1r/pppp1ppp/4pn2/6B1/3P4/5N2/PPP1PPPP/RN1QKB1R b KQkq - 1 3'), [ { from: [1, 2], to: [3, 2] } ]);
+
+// --- Trompowsky Attack (1. d4 Nf6 2. Bg5) ---
+openingBook.set(generateHashFromFEN('rnbqkb1r/pppppppp/5n2/6B1/3P4/8/PPP1PPPP/RN1QKBNR b KQkq - 1 2'), [ { from: [1, 4], to: [3, 4] }, { from: [1, 5], to: [3, 5] } ]);
+
+// --- Budapest Gambit (1. d4 Nf6 2. c4 e5) ---
+openingBook.set(generateHashFromFEN('rnbqkb1r/pppp1ppp/5n2/4p3/2PP4/8/PP2PPPP/RNBQKBNR w KQkq - 0 3'), [ { from: [3, 3], to: [4, 4] } ]);
+
+// --- Benko Gambit (1. d4 Nf6 2. c4 c5 3. d5 b5) ---
+openingBook.set(generateHashFromFEN('rnbqkb1r/p2ppppp/5n2/1ppP4/2P5/8/PP2PPPP/RNBQKBNR w KQkq - 0 4'), [ { from: [2, 1], to: [3, 1] } ]);
+
+
+
+
