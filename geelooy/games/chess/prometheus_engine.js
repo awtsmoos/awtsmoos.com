f463997a4c3c35e88c70ada32f50a5c16e2eb38b
awtsmoos <118651658+awtsmoos@users.noreply.github.com>
2025-10-19 10:39:59 -0400

    B"H Boruch Hashem! Biezras Hashem Blessed is He At Sun Oct 19 2025 10:39:55 GMT-0400 (Eastern Daylight Time), updated prometheus_engine.js

--- geelooy/games/chess/prometheus_engine.js
+++ geelooy/games/chess/prometheus_engine.js
@@ -32,7 +32,7 @@ function buildOpeningBook() {
 // =================================================================
 //                       CONSTANTS & CONFIGURATION
 // =================================================================
-const pieceValues = { p: 100, n: 320, b: 330, r: 500, q: 900, k: 20000 };
+
 const MATE_SCORE = 100000;
 const MATE_IN_MAX_PLY = 64;
 const NULL_MOVE_R = 3;
