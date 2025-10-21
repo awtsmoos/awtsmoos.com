f1c8514c6bfa55877e2bb5d95c900b49607ede28
awtsmoos <118651658+awtsmoos@users.noreply.github.com>
2025-10-21 15:30:11 -0400

    B"H Boruch Hashem! Biezras Hashem Blessed is He update At Tue Oct 21 2025 15:30:10 GMT-0400 (Eastern Daylight Time)

--- geelooy/apps/code/js/importScriptsHack.js
+++ geelooy/apps/code/js/importScriptsHack.js
@@ -49,7 +49,7 @@ export default (workerPath, originalScriptContent) => /*js*/`
                 }
                 console.log('%c[WORKER] ...Woke up!', 'color: #B0C4DE;');
                 Atomics.store(int32, 0, 0);
-                console.log("got result?",result)
+                console.log("got result?",result,scriptCache)
 
                 if (scriptCache.has('error:' + relativePath)) {
                      throw new Error(scriptCache.get('error:' + relativePath));
