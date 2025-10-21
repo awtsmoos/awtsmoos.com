7f653eb20e30f657c4d78ee6e96cf557541ce396
awtsmoos <118651658+awtsmoos@users.noreply.github.com>
2025-10-21 09:56:43 -0400

    B"H
    Update state import and usage in main.js

--- geelooy/apps/code/js/main.js
+++ geelooy/apps/code/js/main.js
@@ -2,13 +2,13 @@
 // FILE: js/main.js
 
 import { App } from './app.js';
-import  state from './state.js'; // Import the new initialization function
+import  {State} from './state.js'; // Import the new initialization function
 
 // This event listener waits for the entire HTML page to be ready.
 document.addEventListener('DOMContentLoaded', () => {
     
     // 1. First, we guarantee that all DOM elements are found and stored.
-    console.log("got state", state); 
+    console.log("got state", State); 
     
     // 2. Only then do we initialize the rest of the application.
     App.initialize();
