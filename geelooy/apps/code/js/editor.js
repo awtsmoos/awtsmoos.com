5096286c283e76bc834bc7ecb6385c79dfb74d34
awtsmoos <118651658+awtsmoos@users.noreply.github.com>
2025-10-21 06:05:10 -0400

    B"H Boruch Hashem! Biezras Hashem Blessed is He At Tue Oct 21 2025 06:05:10 GMT-0400 (Eastern Daylight Time), updated editor.js

---
+++ geelooy/apps/code/js/editor.js
@@ -0,0 +1,56 @@
+// B"H
+// FILE: js/editor.js
+
+import { DOM } from './state.js';
+import { UI } from './ui.js';
+import { StatusBar } from './statusbar.js';
+import pnimi from './pnimi.js'; // Assuming pnimi.js is in the same folder
+
+/**
+ * Editor Module: Manages the main text editor area.
+ */
+export const Editor = {
+    currentHighlighter: null,
+
+    getExt: (name) => {
+        var ld = name.lastIndexOf(".");
+        if (ld < 0) return "";
+        var last = name.substring(ld);
+        return last;
+    },
+
+    setContent: (content = "", filename = "") => {
+        DOM.editor.value = content;
+        UI.updateLineNumbers();
+        StatusBar.updateLanguage(filename);
+        setTimeout(() => { 
+            UI.syncScroll(); 
+            Editor.focus();
+
+            if (Editor.currentHighlighter) {
+                Editor.currentHighlighter.destroy();
+            }
+
+            var ext = Editor.getExt(filename);
+            var real = {
+                ".js": "js",
+                ".css": "css",
+                ".html": "html"
+            };
+            var f = real[ext];
+
+            Editor.currentHighlighter = new pnimi(DOM.editor, f || "js");
+        }, 0);
+    },
+
+    getContent: () => DOM.editor.value,
+
+    getCursorInfo: () => {
+        try {
+            const textLines = DOM.editor.value.substring(0, DOM.editor.selectionStart).split("\n");
+            return { line: textLines.length, col: textLines[textLines.length - 1].length + 1 };
+        } catch (e) { return { line: 1, col: 1 }; }
+    },
+
+    focus: () => DOM.editor.focus(),
+};
