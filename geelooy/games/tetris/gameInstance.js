5df4b14032f6dd5b62e9e12f8ed1a7cdaf396dc3
awtsmoos <118651658+awtsmoos@users.noreply.github.com>
2025-10-21 01:56:19 -0400

    B"H Boruch Hashem! Biezras Hashem Blessed is He At Tue Oct 21 2025 01:56:17 GMT-0400 (Eastern Daylight Time), updated gameInstance.js

--- geelooy/games/tetris/gameInstance.js
+++ geelooy/games/tetris/gameInstance.js
@@ -240,9 +240,20 @@ class GameInstance {
     /**
      * Moves the current piece horizontally.
      */
+    // In gameInstance.js
+
+    /**
+     * Moves the current piece horizontally, now with wall slide effects.
+     */
     move(dir) {
-        if (this.piece && !this.collides(this.piece, { x: dir })) {
+        if (!this.piece) return;
+
+        if (!this.collides(this.piece, { x: dir })) {
+            // Move was successful
             this.piece.x += dir;
+        } else {
+            // THE FIX: Move failed, so trigger the wall slide effect
+            this.effectsEngine.triggerWallSlide(this.piece, dir, this.blockSize, this.viewportTopY);
         }
     }
 
