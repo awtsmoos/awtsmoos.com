f8afed87eeaf332ecabdb99901a74f0d7cf18561
awtsmoos <118651658+awtsmoos@users.noreply.github.com>
2025-10-21 03:28:01 -0400

    B"H Boruch Hashem! Biezras Hashem Blessed is He At Tue Oct 21 2025 03:28:01 GMT-0400 (Eastern Daylight Time), updated particle.js

---
+++ geelooy/games/connect4/particle.js
@@ -0,0 +1,29 @@
+//B"H
+
+class Particle {
+    constructor(x, y, char) {
+        this.x = x;
+        this.y = y;
+        this.char = char;
+        this.size = Math.random() * 20 + 10;
+        this.speedX = Math.random() * 6 - 3;
+        this.speedY = Math.random() * 6 - 3;
+        this.alpha = 1;
+        this.color = `hsl(${Math.random() * 360}, 50%, 50%)`;
+    }
+
+    update() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+        this.alpha -= 0.02;
+    }
+
+    draw(ctx) {
+        ctx.save();
+        ctx.globalAlpha = this.alpha;
+        ctx.fillStyle = this.color;
+        ctx.font = `${this.size}px Arial`;
+        ctx.fillText(this.char, this.x, this.y);
+        ctx.restore();
+    }
+}
