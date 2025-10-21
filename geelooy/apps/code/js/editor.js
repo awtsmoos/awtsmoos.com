1dda48688789e4cc277f7df494fd1c3b93dbc6bf
awtsmoos <118651658+awtsmoos@users.noreply.github.com>
2025-10-21 07:59:55 -0400

    B"H Boruch Hashem! Biezras Hashem Blessed is He update At Tue Oct 21 2025 07:59:54 GMT-0400 (Eastern Daylight Time)

--- geelooy/apps/code/js/editor.js
+++ geelooy/apps/code/js/editor.js
@@ -6,41 +6,89 @@ import { UI } from './ui.js';
 import { StatusBar } from './statusbar.js';
 import pnimi from "/scripts/awtsmoos/coding/pnimi.js";
 
-/**
- * Editor Module: Manages the main text editor area.
- */
+
+
+
+
 export const Editor = {
     currentHighlighter: null,
+    currentObjectURL: null, // To track blob URLs for memory management
 
-    getExt: (name) => {
-        var ld = name.lastIndexOf(".");
-        if (ld < 0) return "";
-        var last = name.substring(ld);
-        return last;
+    _getExt: (name) => {
+        const ld = name.lastIndexOf(".");
+        return (ld < 0) ? "" : name.substring(ld);
+    },
+    
+    _clearPreviewer() {
+        DOM.previewer.innerHTML = '';
+        DOM.previewer.classList.add('hidden');
+        // Revoke the old URL to prevent memory leaks
+        if (this.currentObjectURL) {
+            URL.revokeObjectURL(this.currentObjectURL);
+            this.currentObjectURL = null;
+        }
     },
 
-    setContent: (content = "", filename = "") => {
+    showTextEditor(content = "", filename = "") {
+        this._clearPreviewer();
+        DOM.editorWrapper.classList.remove('hidden');
+
         DOM.editor.value = content;
         UI.updateLineNumbers();
         StatusBar.updateLanguage(filename);
         setTimeout(() => { 
             UI.syncScroll(); 
-            Editor.focus();
+            this.focus();
 
-            if (Editor.currentHighlighter) {
-                Editor.currentHighlighter.destroy();
+            if (this.currentHighlighter) {
+                this.currentHighlighter.destroy();
             }
-
-            var ext = Editor.getExt(filename);
-            var real = {
-                ".js": "js",
-                ".css": "css",
-                ".html": "html"
-            };
-            var f = real[ext];
-
-            Editor.currentHighlighter = new pnimi(DOM.editor, f || "js");
+            const ext = this._getExt(filename);
+            const langMap = { ".js": "js", ".css": "css", ".html": "html" };
+            this.currentHighlighter = new pnimi(DOM.editor, langMap[ext] || "js");
         }, 0);
+    },
+
+    showPreviewer(data, fileInfo) {
+        if (this.currentHighlighter) {
+            this.currentHighlighter.destroy();
+            this.currentHighlighter = null;
+        }
+        DOM.editorWrapper.classList.add('hidden');
+        this._clearPreviewer();
+        DOM.previewer.classList.remove('hidden');
+
+        let url;
+        // Handle raw Blobs/Files from Local FS and data URL objects from GitHub
+        if (data.isBinary) { // GitHub object
+            url = `data:${data.mime};base64,${data.base64Content}`;
+        } else { // Local FS Blob/File
+            url = URL.createObjectURL(data);
+            this.currentObjectURL = url; // Track for cleanup
+        }
+
+        switch(fileInfo.type) {
+            case 'image':
+                DOM.previewer.innerHTML = `<img src="${url}" alt="${fileInfo.name}">`;
+                break;
+            case 'video':
+                DOM.previewer.innerHTML = `<video src="${url}" controls></video>`;
+                break;
+            case 'audio':
+                DOM.previewer.innerHTML = `<audio src="${url}" controls></audio>`;
+                break;
+            case 'pdf':
+                DOM.previewer.innerHTML = `<embed src="${url}" type="application/pdf" />`;
+                break;
+            default: // 'binary' or other unsupported types
+                DOM.previewer.innerHTML = `
+                    <div class="unsupported-message">
+                        <svg class="svg-icon"><use href="#icon-file"></use></svg>
+                        <h3>Binary File</h3>
+                        <p>This file type cannot be previewed.</p>
+                    </div>`;
+                break;
+        }
     },
 
     getContent: () => DOM.editor.value,
@@ -54,3 +102,4 @@ export const Editor = {
 
     focus: () => DOM.editor.focus(),
 };
+
