ddc190b016e90af9a13aba0ae6de2ae9c1fc331a
awtsmoos <118651658+awtsmoos@users.noreply.github.com>
2025-10-22 07:12:28 -0400

    B"H
    Fix syntax error in app.js

--- geelooy/apps/code/js/app.js
+++ geelooy/apps/code/js/app.js
@@ -452,7 +452,7 @@ setupEventListeners() {
             console.log("Fullscreen API not supported by this browser.");
         }
     }
-}
+},
 
     async showSettings() {
         const contentHTML = `
