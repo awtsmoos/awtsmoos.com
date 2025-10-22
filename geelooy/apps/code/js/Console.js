dc99328a93863a4a42d3eaf65e143e10530f1cd2
awtsmoos <118651658+awtsmoos@users.noreply.github.com>
2025-10-22 18:59:06 -0400

    B"H Boruch Hashem! Biezras Hashem Blessed is He update At Wed Oct 22 2025 18:59:06 GMT-0400 (Eastern Daylight Time)

--- geelooy/apps/code/js/Console.js
+++ geelooy/apps/code/js/Console.js
@@ -214,7 +214,7 @@ _renderLogMessage(log) {
             textarea.value = "hello would"//this._prettyPrint(arg);
             
             // STEP 2: Add the textarea directly to the page.
-            contentContainer.appendChild(textarea);
+            document.body.appendChild(textarea);
             
             // STEP 3: Call `new pnimi` on the textarea after it's on the page.
             // We use a timeout to ensure the browser has rendered it first.
