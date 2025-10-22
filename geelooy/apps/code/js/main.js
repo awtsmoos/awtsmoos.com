e727d677f92d2b0bd2ece262ec6d6b44b8235dad
awtsmoos <118651658+awtsmoos@users.noreply.github.com>
2025-10-22 05:49:44 -0400

    B"H

--- geelooy/apps/code/js/main.js
+++ geelooy/apps/code/js/main.js
@@ -8,7 +8,7 @@ import  {State, DOM} from './state.js'; // Import the new initialization functio
 document.addEventListener('DOMContentLoaded', () => {
     
     // 1. First, we guarantee that all DOM elements are found and stored.
-    console.log("got state", State, DOM); 
+   // console.log("got state", State, DOM); 
     
     // 2. Only then do we initialize the rest of the application.
     App.initialize();
