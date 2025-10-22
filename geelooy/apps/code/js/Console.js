32c617d0767be98ad17498c48735f9705f2701f1
awtsmoos <118651658+awtsmoos@users.noreply.github.com>
2025-10-22 18:13:55 -0400

    B"H Boruch Hashem! Biezras Hashem Blessed is He update At Wed Oct 22 2025 18:13:54 GMT-0400 (Eastern Daylight Time)

--- geelooy/apps/code/js/Console.js
+++ geelooy/apps/code/js/Console.js
@@ -225,8 +225,8 @@ _renderLogMessage(log) {
             // STEP 3: Call `new pnimi` on the textarea after it's on the page.
             // We use a timeout to ensure the browser has rendered it first.
             
-                const highlighter = new pnimi(textarea, 'js');
-                highlighter.update(arg);
+              //  const highlighter = new pnimi(textarea, 'js');
+              //  highlighter.update(arg);
                 // Store the instance only for later cleanup. Do not touch it otherwise.
          //       this.outputHighlighters.set(textarea, highlighter); 
            
