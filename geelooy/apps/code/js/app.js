f051f7a7f5c9c900f56aeea71505f8521074e683
awtsmoos <118651658+awtsmoos@users.noreply.github.com>
2025-10-22 14:19:58 -0400

    B"H Boruch Hashem! Biezras Hashem Blessed is He update At Wed Oct 22 2025 14:19:56 GMT-0400 (Eastern Daylight Time)

--- geelooy/apps/code/js/app.js
+++ geelooy/apps/code/js/app.js
@@ -16,6 +16,8 @@ import { FindReplace } from './find-replace.js';
 export const App = {
     getTabString: () => State.useTabs ? '\t' : '    ',
 activeConsole: null, // B"H 
+	consoleInstances: new Map(), // Maps a console tab ID to its Console instance
+    previewIframes: new Map(), // Maps an HTML preview tab ID to its iframe element
     saveSession() {
         const persistableWorkspaces = State.workspaces
             .filter(ws => ws.type === 'github' || ws.type === 'indexeddb')
