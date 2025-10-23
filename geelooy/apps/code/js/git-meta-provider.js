a76b59b59219e05dc1783df3249f0dbedd475852
awtsmoos <118651658+awtsmoos@users.noreply.github.com>
2025-10-23 04:54:01 -0400

    B"H Boruch Hashem! Biezras Hashem Blessed is He update At Thu Oct 23 2025 04:54:01 GMT-0400 (Eastern Daylight Time)

--- geelooy/apps/code/js/git-meta-provider.js
+++ geelooy/apps/code/js/git-meta-provider.js
@@ -25,7 +25,7 @@ export const GitMetaProvider = {
             // 1. Find the start of the object.
             const objectStartIndex = content.indexOf('{');
             if (objectStartIndex === -1) {
-                console.warn("Found ikar.js but it did not contain a valid metadata object.", folderItem);
+                console.warn("Found ikar.js but it did not contain a valid metadata object.", folderItem, content);
                 return null;
             }
 
