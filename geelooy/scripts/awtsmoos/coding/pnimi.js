0314fb296b76fc597afd155fd5cf5ec24dd1b97e
awtsmoos <118651658+awtsmoos@users.noreply.github.com>
2025-10-23 11:21:12 -0400

    B"H Boruch Hashem! Biezras Hashem Blessed is He update At Thu Oct 23 2025 11:21:11 GMT-0400 (Eastern Daylight Time)

--- geelooy/scripts/awtsmoos/coding/pnimi.js
+++ geelooy/scripts/awtsmoos/coding/pnimi.js
@@ -435,12 +435,7 @@ _findMatchingBrace(line, startIndex = 0) {
     
     
     
-    _highlightJS(line, state) {
-    // Divine Guard: A state must always exist.
-    const currentState = state || this._getInitialState();
-    const lang = {
-        keywords: ['const', 'let', 'var', 'function', 'return', 'if', 'else', 'for', 'class', 'new', 'await', 'async', 'import', 'export', 'from', 'while', 'do', 'switch', 'case', 'break'],
-    };
+    
     let html = '';
     let i = 0;
 
