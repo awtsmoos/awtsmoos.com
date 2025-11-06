bfe576840fe8ca46fa2535f17e166a7f2974dab3
awtsmoos <118651658+awtsmoos@users.noreply.github.com>
2025-11-05 20:26:11 -0800

    B"H Boruch Hashem! Biezras Hashem Blessed is He At Wed Nov 05 2025 23:26:09 GMT-0500 (Eastern Standard Time)

--- geelooy/games/migdol/js/ui.js
+++ geelooy/games/migdol/js/ui.js
@@ -161,36 +161,7 @@ game) {
 	}
 }
 
-// It's good practice to move utility functions out
-// Create a new file js/utils.js for this function
-export function isLocationValid(gridX,
-	gridY, game) {
-	if (gridX < 0 || gridX >= (game
-			.canvas.width / TILE_SIZE
-			) || gridY < 0 || gridY >= (
-			game.canvas.height /
-			TILE_SIZE)) {
-		return false;
-	}
-	for (let i = 0; i < game.path
-		.length - 1; i++) {
-		const start = game.path[i];
-		const end = game.path[i + 1];
-		if (start.x === end.x &&
-			gridX === start.x &&
-			gridY >= Math.min(start.y,
-				end.y) && gridY <= Math
-			.max(start.y, end.y))
-		return false;
-		if (start.y === end.y &&
-			gridY === start.y &&
-			gridX >= Math.min(start.x,
-				end.x) && gridX <= Math
-			.max(start.x, end.x))
-		return false;
-	}
-	return !game.towers.some(t => Math
-		.floor(t.x / TILE_SIZE) ===
-		gridX && Math.floor(t.y /
-			TILE_SIZE) === gridY);
-}
+
+
+
+
