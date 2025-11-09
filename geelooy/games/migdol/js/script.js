e7cd2d8dedbd93e4abbfb59f0b2d483115858668
awtsmoos <118651658+awtsmoos@users.noreply.github.com>
2025-11-09 00:46:20 -0800

    B"H Boruch Hashem! Biezras Hashem Blessed is He At Sun Nov 09 2025 03:46:18 GMT-0500 (Eastern Standard Time)

--- geelooy/games/migdol/js/script.js
+++ geelooy/games/migdol/js/script.js
@@ -30,7 +30,7 @@ class Game {
         this.eventMessages = [];
         this.path = this.map.path;
          
-        this.perutas = 500 
+        this.perutas = 500  
         this.health = 500; // Increased for better playability
         
         this.selectedTowerType = null;
