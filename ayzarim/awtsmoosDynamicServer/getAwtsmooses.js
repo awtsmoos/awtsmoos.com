32eddfe6b411316d6e62a80752ed98bac179dff1
awtsmoos <118651658+awtsmoos@users.noreply.github.com>
2025-11-27 15:09:09 -0800

    B"H Updated at 11/27/2025, 6:09:08 PM

--- ayzarim/awtsmoosDynamicServer/getAwtsmooses.js
+++ ayzarim/awtsmoosDynamicServer/getAwtsmooses.js
@@ -12,36 +12,27 @@ var fs = require("fs").promises;
 var path = require("path")
 var getProperContent = require("./getProperContent.js")
 var fetchAwtsmoos = require("./fetchAwtsmoos.js");
+
 class Ayzarim {
 	constructor(dependencies) {
 		this.dependencies = dependencies;
 		this.fetchAwtsmoos = fetchAwtsmoos;
-		this.dependencies.fetchAwtsmoos =
-			this.fetchAwtsmoos.bind(this)
+		this.dependencies.fetchAwtsmoos = this.fetchAwtsmoos.bind(this)
 		this.server = dependencies.self;
 		this.foundAwtsmooses = []
 		this.logs = {}
 
-
-		this.filePath = dependencies
-			.filePath;
-
-        this.parentPath = dependencies 
-            .parentPath;
+		this.filePath = dependencies.filePath;
+		this.parentPath = dependencies.parentPath;
 
 		this.isDirectoryWithIndex = false
 		this.isRealFile = false
-		this.contentType = dependencies
-			.contentType
+		this.contentType = dependencies.contentType
 	}
 
 	errorMessage(...args) {
 		return errorMessage.bind(this)(...args)
 	}
-
-	
-
-
 
 	async doEverything() {
 		return (doEverything.bind(this))()
@@ -49,44 +40,35 @@ class Ayzarim {
 }
 
 async function getPathInfo() {
-
 	var {
-        
 		filePath,
 		awtsRes,
 		response,
 		originalPath,
-		parsedUrl
+		location, // We need this to preserve query params during redirect
+		request
 	} = this.dependencies;
+
 	this.filePath = path.normalize(filePath);
 	awtsRes.ended = false;
 	var doesNotExist = false;
 
 	this.filePaths = filePath
 		.split("\\")
-		
-		
 		.filter(q => q)
 		.join("/")
-		
-		
 		.split("/")
 		.filter(w => w);
-		
 
-	this.fileName = this
-		.filePaths[
-			this
-			.filePaths.length - 1
-		];
+	this.fileName = this.filePaths[this.filePaths.length - 1];
 
 	try {
 		var st = null;
 		try {
 			st = await fs.stat(filePath);
 		} catch (e) {
-			// Soft Retry: If "/login" failed, check if it's a folder by appending slash logic internally
-			// effectively treating them the same without a redirect
+			// Soft Retry: If the path failed, check if it works by treating it as a folder
+			// This allows internal resolution of "/jem" -> "/jem/" without forcing a redirect for POSTs
 			if (e.code === 'ENOENT' && !filePath.endsWith(path.sep)) {
 				try {
 					st = await fs.stat(filePath + path.sep);
@@ -94,21 +76,40 @@ async function getPathInfo() {
 					if (st && st.isDirectory()) filePath = filePath + path.sep;
 				} catch (ex) {}
 			}
-			if (!st) throw e; // If still nothing, throw original error
+			if (!st) throw e;
 		}
 
 		if (st && st.isDirectory()) {
+			// B"H: INTELLIGENT REDIRECT LOGIC
+			// 1. If it's a GET request (Browser navigation), we MUST redirect to add the trailing slash.
+			//    This ensures that relative links (style.css, fetch('api')) resolve to this folder, not the parent.
+			// 2. If it's a POST/PUT/DELETE, we DO NOT redirect.
+			//    Redirecting a POST often causes the body to be lost (301/302) or requires a 307.
+			//    Since we resolved the path internally above, we can just serve the response directly.
+
+			if (!originalPath.endsWith('/') && request.method === 'GET') {
+				var redirectUrl = originalPath + '/';
+
+				// Preserve Query Parameters and Hash
+				if (location && location.search) redirectUrl += location.search;
+				if (location && location.hash) redirectUrl += location.hash;
+
+				response.writeHead(301, {
+					Location: redirectUrl
+				});
+				response.end();
+				awtsRes.ended = true;
+				return false;
+			}
+
 			var indexFilePath = path.join(this.filePath, "index.html");
 			var san = path.normalize(indexFilePath);
 
-			// Logic Change: Check for index.html. 
-			// If found, serve it immediately. NO REDIRECTS.
 			if (await exists(san)) {
 				this.filePath = san;
 				this.isDirectoryWithIndex = true;
 				this.fileName = "index.html";
 			} else {
-				// Directory exists, but no index.html (maybe dynamic directory)
 				this.isDirectoryWithoutIndex = true;
 				this.dependencies.awtsRes.ended = false;
 			}
@@ -123,89 +124,60 @@ async function getPathInfo() {
 	}
 
 	this.dependencies.awtsRes.ended = false;
-	var isReal = (
-		!doesNotExist
-	);
-    
-	var isDynamic = !isReal 
-        || this.isDirectoryWithoutIndex;
+	var isReal = (!doesNotExist);
+
+	var isDynamic = !isReal || this.isDirectoryWithoutIndex;
 	if (isDynamic) {
-
-		this.foundAwtsmooses = await
-		awtsRes.getAwtsmoosInfo(
-            this.filePath,
-            this.parentPath
-        );
-        
+		this.foundAwtsmooses = await awtsRes.getAwtsmoosInfo(
+			this.filePath,
+			this.parentPath
+		);
 	}
-    
-    
-	this.logs.lol = { filePath: this.filePath, fa: this.foundAwtsmooses, isDynamic }
 
+	this.logs.lol = {
+		filePath: this.filePath,
+		fa: this.foundAwtsmooses,
+		isDynamic
+	}
 
-
-	return (
-		!!this.foundAwtsmooses.length ||
-		isReal
-	);
-
+	return (!!this.foundAwtsmooses.length || isReal);
 }
 
 async function doEverything() {
-
 	var {
 		fs,
-
 		awtsRes,
 		response,
 		originalPath,
-		parsedUrl,
 		request,
 		getPostData,
 		getPutData,
 		getDeleteData
 	} = this.dependencies;
 
-
-
 	var iExist = await (getPathInfo.bind(this))();
-	
 
 	if (!iExist) {
+		if (this.fileName && this.fileName.startsWith("@")) {
+			var tr = "/@/" + this.fileName.substring(1)
+			var res = await this.fetchAwtsmoos(tr, {
+				superSecret: true
+			})
 
-
-		if (this
-			.fileName.startsWith("@")) {
-			var tr = "/@/" + this
-				.fileName.substring(1)
-
-
-			var res = await this.fetchAwtsmoos(
-				tr, {
-					superSecret: true
-				}
-			)
-			
-			if(res) {
-				if(typeof(res) == "object") {
+			if (res) {
+				if (typeof (res) == "object") {
 					res = JSON.stringify(res);
-					response.setHeader("content-type",
-						"application/json; charset=utf-8"
-					)
+					response.setHeader("content-type", "application/json; charset=utf-8")
 				}
 				response.end(res);
-			}
-			else return errorMessage.bind(this)({
+			} else return errorMessage.bind(this)({
 				message: "Content empty",
 				code: "EMPTY"
 			})
 			return;
 		}
 
-		
-
-
-        try {
+		try {
 			return errorMessage.bind(this)({
 				message: "Dynamic route not found",
 				code: "DYN_ROUTE_NOT_FOUND",
@@ -214,91 +186,47 @@ async function doEverything() {
 				},
 				logs: this.logs
 			});
-		} catch(e) {
-			
-		}
+		} catch (e) {}
 	}
 
-
-
 	if (this.isDirectoryWithIndex) {
-
-		this
-			.contentType = "text/html";
+		this.contentType = "text/html";
 	}
 
 	var didThisPathAlready = false;
 
-	if (request.method.toUpperCase() == "POST") {
-		await getPostData();
-	}
+	if (request.method.toUpperCase() == "POST") await getPostData();
+	if (request.method.toUpperCase() == "PUT") await getPutData();
+	if (request.method.toUpperCase() == "DELETE") await getDeleteData();
 
-	if (request.method.toUpperCase() == "PUT") {
-		await getPutData();
-	}
-
-	if (request.method.toUpperCase() == "DELETE") {
-		await getDeleteData();
-	}
-
-	if (
-		this.foundAwtsmooses.length &&
-		!this.isDirectoryWithIndex
-	) {
+	if (this.foundAwtsmooses.length && !this.isDirectoryWithIndex) {
 		try {
-
-			didThisPathAlready = await
-			awtsRes.doAwtsmooses({
+			didThisPathAlready = await awtsRes.doAwtsmooses({
 				foundAwtsmooses: this.foundAwtsmooses,
 				filePath: this.filePath,
 				extraInfo: {
-					fetchAwtsmoos:this.fetchAwtsmoos
+					fetchAwtsmoos: this.fetchAwtsmoos
 				}
 			});
-		} catch(e) {
+		} catch (e) {
 			return {
-				BH:
-				"yo",
+				BH: "yo",
 				AwtsmoosError: {
 					message: e.message,
 					stack: e.stack
 				}
 			}
 		}
-
-
 	}
 
-	
-	if (
-		didThisPathAlready === false
-	) {
-		if (
-
-			this
-			.isDirectoryWithIndex ||
-			this
-			.isRealFile
-
-		) {
-
-			var startsWithAw = this
-				.fileName.startsWith("_awtsmoos")
-			
-			if (
-				!startsWithAw ||
-				request.superSecret
-			) {
-
-
+	if (didThisPathAlready === false) {
+		if (this.isDirectoryWithIndex || this.isRealFile) {
+			var startsWithAw = this.fileName.startsWith("_awtsmoos")
+			if (!startsWithAw || request.superSecret) {
 				return await doFileResponse.bind(this)();
-
 			} else {
-				return errorMessage.bind(this)(
-					"You're not allowed to see that!"
-				)
+				return errorMessage.bind(this)("You're not allowed to see that!")
 			}
-
 		} else {
 			return errorMessage.bind(this)({
 				message: "Invalid Dynamic Route",
@@ -306,33 +234,26 @@ async function doEverything() {
 				more: {
 					didThisPathAlready,
 					foundAwtsmooses: this.foundAwtsmooses,
-					idwi:this.isDirectoryWithIndex,
+					idwi: this.isDirectoryWithIndex,
 					logs: this.logs
-
 				}
-
 			})
-
 		}
-
-	} else if(didThisPathAlready.error) {
+	} else if (didThisPathAlready.error) {
 		return errorMessage.bind(this)({
 			message: "actual error in route computation!",
 			code: "ROUTE_ERROR",
-			error:didThisPathAlready.error,
+			error: didThisPathAlready.error,
 			more: {
 				didThisPathAlready,
 				logs: this.logs,
 				foundAwtsmooses: this.foundAwtsmooses
-
 			}
 		})
 	}
 
-
 	if (didThisPathAlready.c) {
-		var res = didThisPathAlready
-			.responseInfo;
+		var res = didThisPathAlready.responseInfo;
 
 		try {
 			response.setHeader('Vary', 'Cookie');
@@ -346,47 +267,31 @@ async function doEverything() {
 			}
 			var ar = res.actualResponse;
 			var bin = null;
-			if(Buffer.isBuffer(ar)) {
-				/*response.setHeader(
-					"content-type",
-					"application/octet-stream"
-				);*/
+			if (Buffer.isBuffer(ar)) {
 				bin = ar;
-			} else if (res.actualResponse
-				.contentType) {
+			} else if (res.actualResponse.contentType) {
 				response.setHeader(
 					"content-type",
-					res.actualResponse
-					.contentType+"; charset=utf-8"
+					res.actualResponse.contentType + "; charset=utf-8"
 				);
-			} else {
-				/*response.setHeader(
-					"content-type",
-					"text/html; charset=utf-8"
-				);*/
 			}
 
-			
 			var con = bin || res.actualResponse.content;
 			if (con || con === "undefined" || con === "null") {
-				if(Buffer.isBuffer(con)) {
+				if (Buffer.isBuffer(con)) {
 					//do nothing
-				} else if(typeof(con) == "object") {
+				} else if (typeof (con) == "object") {
 					con = JSON.stringify(con)
-				} else if (typeof(con) != "string") {
+				} else if (typeof (con) != "string") {
 					con += ""
 				}
-		
-		
-				response.end(
-					con
-				)
+				response.end(con)
 			} else {
-				console.log("GOING to DO",con, res)
+				console.log("GOING to DO", con, res)
 				return errorMessage.bind(this)({
 					message: "No Awtsmoos Response",
 					code: "NO_AWTS_RESP",
-					con: typeof(con)
+					con: typeof (con)
 				});
 			}
 		} catch (e) {
@@ -401,7 +306,6 @@ async function doEverything() {
 				didThisPathAlready,
 				logs: this.logs,
 				foundAwtsmooses: this.foundAwtsmooses
-
 			}
 		})
 	} else if (didThisPathAlready.isPrivate) {
@@ -415,11 +319,7 @@ async function doEverything() {
 			code: "NOT_FOUND"
 		})
 	}
-
 }
-
-
-
 
 async function doFileResponse() {
 	var {
@@ -430,142 +330,92 @@ async function doFileResponse() {
 		binaryMimeTypes,
 		fetchAwtsmoos
 	} = this.dependencies;
-	
+
 	try {
-		if (
-			request.method == "GET" &&
-			//request.headers['awtsmoos-file-status'] &&
-			request.isAwtsmoosFileStatusRequest
-		) {
-		        try {
-		            // The path to the static file is already in this.filePath
-		            const stats = await fs.stat(this.filePath);
-		            
-		            // A static file has no separate "logic", so we only return dataModified
-		            const result = {
-		                dataModified: stats.mtime.getTime()
-		            };
-		
-		            response.setHeader('Content-Type', 'application/json; charset=utf-8');
-		            response.end(JSON.stringify(result));
-		            
-		            // IMPORTANT: Stop execution here to prevent reading the actual file
-		            return; 
-		        } catch (error) {
-		            // This is unlikely to happen, but it's good practice
-		            console.error("Error getting file stats for static file:", error);
-		            return errorMessage.bind(this)({
-		                message: "Could not get file status for static resource.",
-		                code: "STATIC_STAT_ERROR"
-		            });
-		        }
-		    }
+		if (request.method == "GET" && request.isAwtsmoosFileStatusRequest) {
+			try {
+				const stats = await fs.stat(this.filePath);
+				const result = {
+					dataModified: stats.mtime.getTime()
+				};
+				response.setHeader('Content-Type', 'application/json; charset=utf-8');
+				response.end(JSON.stringify(result));
+				return;
+			} catch (error) {
+				console.error("Error getting file stats for static file:", error);
+				return errorMessage.bind(this)({
+					message: "Could not get file status for static resource.",
+					code: "STATIC_STAT_ERROR"
+				});
+			}
+		}
 		let content;
 
 		if (binaryMimeTypes.includes(this.contentType)) {
-			// If the file is a binary file, read it as binary.
 			content = await fs.readFile(this.filePath);
 			this.isBinary = true;
 		} else {
-			// Otherwise, read the file as 'utf-8' text and process it as a template.
 			var textContent = await fs.readFile(this.filePath, 'utf-8');
-			var last = this.filePath
-				.split("\\")
-				.join("/").split("/").pop()
+			var last = this.filePath.split("\\").join("/").split("/").pop()
 			var ext = last.split(".")[1];
-			if(
-				!(ext == "html" ||
-				this.isDirectoryWithIndex)
-			) {
-				/*content = "Hello! " 
-					+this.isDirectoryWithIndex+ ext + " " + last
-				
-				*/
+			if (!(ext == "html" || this.isDirectoryWithIndex)) {
 				content = textContent;
 			} else {
-				var ei/*extra info*/ = request.yeser/*extra*/;
-				if(!(typeof(ei) == "object" && ei)) {
-					ei = {}
-					//asdf
-				}
-				
+				var ei = request.yeser;
+				if (!(typeof (ei) == "object" && ei)) ei = {}
 				ei.fetchAwtsmoos = fetchAwtsmoos;
-				var temp  = await template(textContent, ei);
+				var temp = await template(textContent, ei);
 				content = temp
-				//content = this.isDirectoryWithIndex
 			}
 		}
-
-		// Send the processed content back to the client
 
 		content = setProperContent.bind(this)(
 			content,
 			this.contentType,
 			this.isBinary
 		);
-		
-		response.end(content);
 
+		response.end(content);
 		return;
 	} catch (errors) {
-		// If there was an error, send a 500 response and log the error
 		console.error(errors);
-		return errorMessage.bind(this)(
-			errors
-		) 
+		return errorMessage.bind(this)(errors)
 	}
-
 }
 
-
-
 function setProperContent(content, contentType, isBinary = false) {
-	var { response } = this.dependencies;
-
+	var {
+		response
+	} = this.dependencies;
 	var cnt = getProperContent(content, contentType, isBinary);
-
-	// Use the contentType returned from the helper function.
 	if (cnt.contentType) {
 		try {
-           
 			response.setHeader('Content-Type', cnt.contentType + (isBinary ? '' : '; charset=utf-8'));
-		} catch(e){}
+		} catch (e) {}
 	}
 	return cnt.content;
 }
-
 
 function errorMessage(custom) {
 	var {
 		response
 	} = this.dependencies;
+	try {
 		try {
-			try {
-				response.setHeader("content-type", "application/json; charset=utf-8");
-			} catch(e){}
-			try {
-				response.end(JSON.stringify({
-					BH: "B\"H",
-					error: custom || "Not found"
-				}));
-			} catch(e) {}
-		} catch (e) {
-			console.log(e)
-		}
-
-
-		return true;
-	
+			response.setHeader("content-type", "application/json; charset=utf-8");
+		} catch (e) {}
+		try {
+			response.end(JSON.stringify({
+				BH: "B\"H",
+				error: custom || "Not found"
+			}));
+		} catch (e) {}
+	} catch (e) {
+		console.log(e)
+	}
+	return true;
 }
 
-
-
-/**
- * The "Binah", understanding of whether a file exists at the given file path.
- * 
- * @param {string} filePath - The path to the file, our "Malkhut", sovereignty over the file system.
- * @returns {boolean} True if the file exists, false otherwise.
- */
 async function exists(filePath) {
 	try {
 		await fs.access(filePath);
