7549eb94bc386fd2e792ffa59bc60e1b40cb8d32
awtsmoos <118651658+awtsmoos@users.noreply.github.com>
2025-11-27 21:50:49 -0800

    B"H Updated at 11/27/2025, 9:50:44 PM

--- ayzarim/awtsmoosDynamicServer/awtsmoosAi.js
+++ ayzarim/awtsmoosDynamicServer/awtsmoosAi.js
@@ -19,37 +19,37 @@ const GEMINI_CONFIG = {
             rpm: 10,       // Requests Per Minute
             tpm: 250000,   // Tokens Per Minute
             rpd: 250,      // Requests Per Day
-            priority: 1    // Default Leader
+            priority: 6   
         },
         "gemini-2.5-flash-lite": {
             rpm: 15,
             tpm: 250000,
             rpd: 1000,     // High volume backup
-            priority: 2
+            priority: 5
         },
         "gemini-2.0-flash": {
             rpm: 15,
             tpm: 1000000,
             rpd: 200,
-            priority: 3
+            priority: 4
         },
         "gemini-2.0-flash-lite": {
             rpm: 30,
             tpm: 1000000,
             rpd: 200,
-            priority: 4
+            priority: 3
         },
         "gemini-2.5-flash-preview": {
             rpm: 10,
             tpm: 250000,
             rpd: 250,
-            priority: 5
+            priority: 2
         },
          "gemini-2.5-flash-lite-preview": {
             rpm: 15,
             tpm: 250000,
             rpd: 1000,
-            priority: 6
+            priority: 1
         }
     }
 };
