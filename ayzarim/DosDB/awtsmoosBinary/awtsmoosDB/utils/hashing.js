765bb15df8ddc64fd4700842f9b0517d7ec9cb2d
awtsmoos <118651658+awtsmoos@users.noreply.github.com>
2025-11-30 23:20:06 -0800

    B"H Updated at 11/30/2025, 11:19:55 PM (Deletions)

--- ayzarim/DosDB/awtsmoosBinary/awtsmoosDB/utils/hashing.js
+++
@@ -1,7 +0,0 @@
-// B"H
-const crypto = require("crypto");
-function hashKey(key, size) {
-    const hash = crypto.createHash('md5').update(key).digest();
-    return hash.readUInt32BE(0) % size;
-}
-module.exports = { hashKey };
