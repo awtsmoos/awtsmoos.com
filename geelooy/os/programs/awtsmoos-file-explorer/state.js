0558fcd315e54feacc3f426b2c89461b4a2c5f46
awtsmoos <118651658+awtsmoos@users.noreply.github.com>
2025-12-09 17:53:52 -0800

    B"H Updated at 12/9/2025, 5:53:50 PM

---
+++ geelooy/os/programs/awtsmoos-file-explorer/state.js
@@ -0,0 +1,8 @@
+// B"H
+export const createState = (initialPath) => ({
+    currentPath: initialPath || '/',
+    viewMode: 'icons',
+    sort: { by: 'name', order: 'asc' },
+    columnWidths: ['2fr', '1fr', '1fr'],
+    selectionMode: false
+});  
