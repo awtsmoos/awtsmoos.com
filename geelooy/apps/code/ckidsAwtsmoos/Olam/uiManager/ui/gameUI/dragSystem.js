ba006fc0ac7fd2ac805034cb21742a1448bfd183
awtsmoos <118651658+awtsmoos@users.noreply.github.com>
2026-04-29 23:55:17 -0400

    B"H Manifesting changes.

---
+++ geelooy/apps/code/ckidsAwtsmoos/Olam/uiManager/ui/gameUI/dragSystem.js
@@ -0,0 +1,12 @@
+
+// B"H
+/**
+ * @file dragSystem.js
+ * @description
+ * 🚪 THE GATEWAY REDIRECT 🚪
+ * 
+ * To preserve Seder Hishtalshelus (the chain of realms), we do not sever the old paths.
+ * Instead, we route the legacy import directly to the newly formed, intensely organized
+ * sub-folder system.
+ */
+export { default } from "./drag/index.js";
