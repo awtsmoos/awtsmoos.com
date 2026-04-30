95d48bebeb57155bdf9d5b00e0911039db3fefbe
awtsmoos <118651658+awtsmoos@users.noreply.github.com>
2026-04-30 00:04:29 -0400

    B"H Manifesting changes.

---
+++ geelooy/games/mitzvahWorld/index.js
@@ -0,0 +1,19 @@
+
+/**
+ * @file index.js
+ * @description
+ * THE SINGULARITY POINT (BEREISHIS)
+ * 
+ * Chapter 1: The Gathering of the Waters.
+ * In this moment, we nullify the chaotic 2D "vessels" of the previous state.
+ * We return the soul of the application to the 3D Ikar, where the 
+ * Chossid model and the procedural worlds reside in the potentiality of the Awtsmoos.
+ * 
+ * The 2D logic was a "Klipah" (shell) that obscured the depth of the 3D reality.
+ * We now break that shell and draw down the true light.
+ */
+
+// B"H: Directing all potential energy to the 3D entry point
+import "./ckidsAwtsmoos/ikar.js";
+
+console.log("B\"H - 🌌 The 3D Heavens and Earth are being established.");
