28dd131b2f2e93f23c9c5a976012c346b526887f
awtsmoos <118651658+awtsmoos@users.noreply.github.com>
2026-05-04 16:18:51 -0700

    B"H Manifesting changes.

---
+++ geelooy/games/ohr-hagnuz/src/data/maps/ChunkSouthWest.js
@@ -0,0 +1,16 @@
+
+/**
+ * B"H
+ * Sustained exclusively via the continuous flow of speech maintaining "Earth" logic.
+ * Every `{x, y}` struct proves the nullification of parts contributing simply to the whole dimension safely.
+ */
+export const ChunkSouthWest = [
+    {x:0, y:8, b:'G_T'}, {x:1, y:8, b:'G_T'}, {x:2, y:8, b:'G_T'}, {x:3, y:8, b:'G_T'}, {x:4, y:8, b:'G_T'}, {x:5, y:8, b:'G_T'}, {x:6, y:8, b:'G_T'}, {x:7, y:8, b:'G_T'},
+    {x:0, y:9, b:'G_T'}, {x:1, y:9, b:'G_T'}, {x:2, y:9, b:'G_T'}, {x:3, y:9, b:'G_T'}, {x:4, y:9, b:'G_T'}, {x:5, y:9, b:'G_T'}, {x:6, y:9, b:'G_T'}, {x:7, y:9, b:'G_T'},
+    {x:0, y:10, b:'G_T'}, {x:1, y:10, b:'G_T'}, {x:2, y:10, b:'G_T'}, {x:3, y:10, b:'G_T'}, {x:4, y:10, b:'G_T'}, {x:5, y:10, b:'G_T'}, {x:6, y:10, b:'G_T'}, {x:7, y:10, b:'G_T'},
+    {x:0, y:11, b:'G_T'}, {x:1, y:11, b:'G_T'}, {x:2, y:11, b:'G_T'}, {x:3, y:11, b:'G_T'}, {x:4, y:11, b:'G_T'}, {x:5, y:11, b:'G_T'}, {x:6, y:11, b:'G_T'}, {x:7, y:11, b:'G_T'},
+    {x:0, y:12, b:'G_T'}, {x:1, y:12, b:'G_T'}, {x:2, y:12, b:'G_T'}, {x:3, y:12, b:'G_T'}, {x:4, y:12, b:'G_T'}, {x:5, y:12, b:'G_T'}, {x:6, y:12, b:'G_T'}, {x:7, y:12, b:'G_T'},
+    {x:0, y:13, b:'G_T'}, {x:1, y:13, b:'G_T'}, {x:2, y:13, b:'G_T'}, {x:3, y:13, b:'G_T'}, {x:4, y:13, b:'G_T'}, {x:5, y:13, b:'G_T'}, {x:6, y:13, b:'G_T'}, {x:7, y:13, b:'G_T'},
+    {x:0, y:14, b:'G_T'}, {x:1, y:14, b:'G_T'}, {x:2, y:14, b:'G_T'}, {x:3, y:14, b:'G_T'}, {x:4, y:14, b:'G_T'}, {x:5, y:14, b:'G_T'}, {x:6, y:14, b:'G_T'}, {x:7, y:14, b:'G_T'},
+    {x:0, y:15, b:'G_T'}, {x:1, y:15, b:'G_T'}, {x:2, y:15, b:'G_T'}, {x:3, y:15, b:'G_T'}, {x:4, y:15, b:'G_T'}, {x:5, y:15, b:'G_T'}, {x:6, y:15, b:'G_T'}, {x:7, y:15, b:'G_T'}
+];
---
+++ geelooy/games/ohr-hagnuz/src/data/maps/Chunks/ChunkBeta.js
@@ -0,0 +1,34 @@
+
+/**
+ * B"H
+ * ChunkBeta: The Eastern Expanse of Asiyah.
+ * 
+ * Poetry: The Stretching of the Tent.
+ * In the realm of Asiyah, the boundaries extend towards the East (Mizrach).
+ * These ASCII Otiot (letters) are vessels that hold the greenery and the strength of the trees.
+ * Without His constant speech, this 20x20 grid would revert to absolute nothingness.
+ * 
+ * @module src/data/Maps/Chunks/ChunkBeta
+ */
+export const ChunkBeta = [
+    "TTTTTTTTTTTTTTTTTTTT",
+    "1111111111111111111T",
+    "1111111111111111111T",
+    "1111111111111111111T",
+    "1111111111111111111T",
+    "1111111111111111111T",
+    "1111111111111111111T",
+    "1111111111111111111T",
+    "1111111111111111111T",
+    "1111111111111111111T",
+    "1111111111111111111T",
+    "1111111111111111111T",
+    "1111111111111111111T",
+    "1111111111111111111T",
+    "1111111111111111111T",
+    "1111111111111111111T",
+    "1111111111111111111T",
+    "1111111111111111111T",
+    "1111111111111111111T",
+    "11111111111111111111"
+];
