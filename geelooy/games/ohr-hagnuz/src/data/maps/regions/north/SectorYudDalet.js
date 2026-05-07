2261f43d50d9e01f558d1a918509993b00b3b6e9
awtsmoos <118651658+awtsmoos@users.noreply.github.com>
2026-05-06 21:25:48 -0400

    B"H Manifesting changes.

---
+++ geelooy/games/ohr-hagnuz/src/data/maps/regions/north/SectorYudDalet.js
@@ -0,0 +1,24 @@
+
+/**
+ * B"H
+ * @chapter Sector Yud-Dalet - The Frozen North
+ * @description
+ * Everything is covered in snow ('*') and the trees are frozen ('🎄').
+ * The Golem of Ice ('⛄') guards the absolute limits of Keter.
+ */
+export const SectorYudDalet = [
+    "TTTTTTTTTTTTTTTTTTTTTTTTT",
+    "T***********************T",
+    "T***********************T",
+    "T****🎄***********🎄****T",
+    "T***********************T",
+    "T***********************T",
+    "T**********⛄***********T",
+    "T***********************T",
+    "T***********************T",
+    "T***********************T",
+    "T****🎄***********🎄****T",
+    "T***********22**********T",
+    "T***********22**********T",
+    "TTTTTTTTTTTT⟱⟱TTTTTTTTTTT"
+];
---
+++ geelooy/games/ohr-hagnuz/src/data/maps/regions/south/SectorYudGimmel.js
@@ -0,0 +1,23 @@
+
+/**
+ * B"H
+ * @chapter Sector Yud-Gimmel - The Southern Desert
+ * @description
+ * An arid expanse where water (Remez) is scarce.
+ */
+export const SectorYudGimmel = [
+    "TTTTTTTTTTTT⍐⍌TTTTTTTTTTT",
+    "T...........22..........T",
+    "T...........22..........T",
+    "T...........22..........T",
+    "T.....🌵....22....🌵....T",
+    "T...........22..........T",
+    "T...........22..........T",
+    "T.....🤲....22..........T",
+    "T...........22..........T",
+    "T...........22....🦂....T",
+    "T....🌵.....22..........T",
+    "T...........22..........T",
+    "T...........22..........T",
+    "TTTTTTTTTTTT⤒⤓TTTTTTTTTTT"
+];
