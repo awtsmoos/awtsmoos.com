ba21a331bd590ea6d9c7a2ae009bd5431f58722b
awtsmoos <118651658+awtsmoos@users.noreply.github.com>
2026-05-14 05:59:42 -0400

    B"H Manifesting changes.

--- geelooy/api/oauth/data/clients.js
+++ geelooy/api/oauth/data/clients.js
@@ -17,7 +17,7 @@ const oauthClients = {
     clientSecret: "",
     autoApprove: false,
     accessTokenSeconds: 3600,
-    defaultScope: "profile tunnel.read",
+    defaultScope: "profile tunnel.read tunnel.write tunnel.command tunnel.browser",
     scopes: ["profile", "tunnel.read", "tunnel.write"],
     exampleRedirectUri: "https://chatgpt.com/aip/gpts/oauth/callback",
     redirectUris: [
@@ -30,6 +30,8 @@ const oauthClients = {
       "https://*.openai.com",
       "https://*.openai.com/",
       "https://*.openai.com/*",
+	  "https://chat.openai.com/aip/g-01199e270e92ece8406917b184a3f985d4e31220/oauth/callback",
+      "https://chatgpt.com/aip/g-01199e270e92ece8406917b184a3f985d4e31220/oauth/callback",
 		"https://awtsmoos.com/api/oauth/callback-test",
     ]
   }
