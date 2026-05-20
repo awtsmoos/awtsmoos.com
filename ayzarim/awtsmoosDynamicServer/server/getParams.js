0bd830acdaaaf38a2283a96dde6f889bcc1b1aaf
awtsmoos <118651658+awtsmoos@users.noreply.github.com>
2026-05-20 00:33:25 -0400

    B"H Manifesting changes.

--- ayzarim/awtsmoosDynamicServer/server/getParams.js
+++ ayzarim/awtsmoosDynamicServer/server/getParams.js
@@ -1,14 +1,32 @@
-
 // B"H
+
+const PLAIN_TEXT_QUERY_KEYS = new Set([
+  "content",
+  "find",
+  "query",
+  "replace",
+  "body",
+  "command",
+  "scriptText",
+  "text",
+  "expression",
+  "testCode",
+  "html"
+]);
 
 /**
  * B"H
- * Parses one query value.
+ * Parses one query value without devouring sacred plain text.
  *
+ * @param {string} key Query key.
  * @param {string} value Raw value.
  * @returns {unknown} JSON value or raw string.
  */
-function parseQueryValue(value) {
+function parseQueryValue(key, value) {
+  if (PLAIN_TEXT_QUERY_KEYS.has(String(key))) {
+    return value;
+  }
+
   try {
     return JSON.parse(value);
   } catch (e) {
@@ -27,10 +45,10 @@ function parseGetParams(fullUrl) {
   const params = Object.fromEntries(fullUrl.searchParams.entries());
 
   for (const key of Object.keys(params)) {
-    params[key] = parseQueryValue(params[key]);
+    params[key] = parseQueryValue(key, params[key]);
   }
 
   return params;
 }
 
-module.exports = { parseGetParams };
+module.exports = { parseGetParams, parseQueryValue, PLAIN_TEXT_QUERY_KEYS };
